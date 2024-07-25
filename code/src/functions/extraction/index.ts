ee856ec67b2fd8e98704bbf1c9eee59972946a74
radovan-jorgic <147607814+radovan-jorgic@users.noreply.github.com>
2024-07-25 10:41:55 +0200

    Update template to use latest library (#5)
    Update ADaaS Template to latest version of ADaaS library.

--- code/src/functions/extraction/index.ts
+++ code/src/functions/extraction/index.ts
@@ -1,10 +1,10 @@
-import { AirdropEvent, DemoExtractor } from '@devrev/ts-adaas';
+import { AirdropEvent, createAdapter, DemoExtractor } from '@devrev/ts-adaas';
 
 const run = async (events: AirdropEvent[]) => {
   for (const event of events) {
-    console.log('Event: ' + JSON.stringify(event));
-    const extractor = new DemoExtractor();
-    await extractor.run(event);
+    const adapter = await createAdapter<object>(event, {});
+    const demoExtractor = new DemoExtractor(event, adapter);
+    await demoExtractor.run();
   }
 };
 
