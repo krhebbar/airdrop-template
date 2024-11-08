1996a9c0e35247a49ed0893b1d936b4830ad5f44
radovan-jorgic <147607814+radovan-jorgic@users.noreply.github.com>
2024-11-08 14:04:24 +0100

    Support for new ADaaS SDK workers architecture (#14)

--- code/src/functions/extraction/index.ts
+++ code/src/functions/extraction/index.ts
@@ -1,10 +1,44 @@
-import { AirdropEvent, createAdapter, DemoExtractor } from '@devrev/ts-adaas';
+import { AirdropEvent, EventType, spawn } from '@devrev/ts-adaas';
+
+interface DummyExtractorState {
+  issues: { completed: boolean };
+  users: { completed: boolean };
+  attachemnts: { completed: boolean };
+}
+
+const initialState: DummyExtractorState = {
+  issues: { completed: false },
+  users: { completed: false },
+  attachemnts: { completed: false },
+};
+
+function getWorkerPerExtractionPhase(event: AirdropEvent) {
+  let path;
+  switch (event.payload.event_type) {
+    case EventType.ExtractionExternalSyncUnitsStart:
+      path = __dirname + '/workers/external-sync-units-extraction';
+      break;
+    case EventType.ExtractionMetadataStart:
+      path = __dirname + '/workers/metadata-extraction';
+      break;
+    case EventType.ExtractionDataStart:
+    case EventType.ExtractionDataContinue:
+      path = __dirname + '/workers/data-extraction';
+      break;
+  }
+  return path;
+}
 
 const run = async (events: AirdropEvent[]) => {
   for (const event of events) {
-    const adapter = await createAdapter<object>(event, {});
-    const demoExtractor = new DemoExtractor(event, adapter);
-    await demoExtractor.run();
+    const file = getWorkerPerExtractionPhase(event);
+    await spawn<DummyExtractorState>({
+      event,
+      initialState,
+      workerPath: file,
+    });
+
+    console.log('Finished extraction for event: ', event.payload.event_type);
   }
 };
 
--- code/src/functions/install_initial_domain_mapping/index.ts
+++ code/src/functions/install_initial_domain_mapping/index.ts
@@ -1,14 +1,13 @@
-import { AirdropEvent, installInitialDomainMapping } from '@devrev/ts-adaas';
+import { installInitialDomainMapping } from '@devrev/ts-adaas';
 
-import initialDomainMapping from '../extraction/initial_domain_mapping.json';
+import initialDomainMapping from '../extraction/dummy-extractor/initial_domain_mapping.json';
 
-const run = async (events: AirdropEvent[]) => {
+const run = async (events: any[]) => {
   for (const event of events) {
     try {
       await installInitialDomainMapping(event, initialDomainMapping);
     } catch (error) {
       console.error('Failed to install initial domain mappings', error);
-      throw error;
     }
   }
 };
