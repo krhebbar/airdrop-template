d8dcc165b3616234f7bc2c5f9ce8a801b86e32aa
Patricija Brečko <patricija.brecko@devrev.ai>
2025-06-20 09:20:22 +0200

    Update runner.ts and Airdrop SDK to 1.5.1 (#41)
    ## Description
    - Update SDK library version to 1.5.1.
    - Update `runner.ts` file.
    
    ## DevRev issue
    https://app.devrev.ai/devrev/works/ISS-185663
    
    ## Documentation PR
    `no-docs`

--- code/test/runner.ts
+++ code/test/runner.ts
@@ -32,6 +32,17 @@ import {
   SnapInsSystemUpdateResponse,
 } from './types';
 
+
+import {
+  Context as SnapInContext,
+  ExecuteOperationResult,
+  ExecuteOperationResult_SerializationFormat,
+  ExecutionMetadata,
+  FunctionExecutionError,
+  FunctionInput,
+  OperationOutput,
+} from '@devrev/typescript-sdk/dist/snap-ins';
+
 const app: Express = express();
 app.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }));
 
@@ -120,21 +131,24 @@ async function handleEvent(events: any[], isAsync: boolean, resp: Response) {
           console.error(error.err_msg);
           receivedError = true;
         } else {
-          result = run(f, [event]);
+          result = await run(f, [event]);
         }
       } catch (e) {
         error = { error: e } as FunctionError;
         console.error(e);
       }
 
-      // post processing. result is updated in the function
-      await postRun(event, error, result);
+      // Any common post processing goes here. The function returns
+      // only if the function execution was by an operation
     }
+    const opResult = await postRun(event, error, result);
 
     // Return result.
     let res: ExecutionResult = {};
 
-    if (result !== undefined) {
+    if (opResult !== undefined) {
+      res.function_result = opResult;
+    } else if (result !== undefined) {
       res.function_result = result;
     }
 
@@ -152,11 +166,16 @@ async function handleEvent(events: any[], isAsync: boolean, resp: Response) {
 // post processing
 async function postRun(event: any, handlerError: HandlerError, result: any) {
   console.debug('Function execution complete');
+  // Check if the function was invoked by an operation.
+  if (isInvokedFromOperation(event)) {
+    return handleOperationInvocationResult(event, handlerError, result);
+  }
   if (isActivateHook(event)) {
     handleActivateHookResult(event, handlerError, result);
   } else if (isDeactivateHook(event)) {
     handleDeactivateHookResult(event, handlerError, result);
   }
+  return undefined
 }
 
 function isActivateHook(event: any): boolean {
@@ -165,6 +184,10 @@ function isActivateHook(event: any): boolean {
 
 function isDeactivateHook(event: any): boolean {
   return event.execution_metadata.event_type === 'hook:snap_in_deactivate';
+}
+
+function isInvokedFromOperation(event: any): boolean {
+  return event.execution_metadata.operation_slug !== undefined;
 }
 
 function handleActivateHookResult(event: any, handlerError: HandlerError, result: any) {
@@ -261,4 +284,81 @@ function getDeactivateHookResult(input: any): DeactivateHookResult {
     }
   }
   return res;
+}
+
+async function handleOperationInvocationResult(
+  event: any,
+  handlerError: HandlerError,
+  result: any,
+): Promise<ExecuteOperationResult> {
+  if (result === undefined) {
+    result = generateOperationOutputFromError(handlerError);
+  }
+
+  const operationMethod = getOperationExecutionOperationMethod(event) || '';
+  if (operationMethod === 'OperationMethod_NameEnumSchemaHandler') {
+    return createExecuteOperationResult(result, ExecuteOperationResult_SerializationFormat.JSON);
+  }
+
+  return createExecuteOperationResult(result, ExecuteOperationResult_SerializationFormat.Proto);
+}
+
+function generateOperationOutputFromError(handlerError: HandlerError): OperationOutput {
+  const errorDetails: FailedExecutionError = {
+    source: FailedExecutionErrorSource.DeveloperFunction,
+    error:
+      handlerError instanceof FunctionExecutionError
+        ? handlerError
+        : new FunctionExecutionError((handlerError as unknown as Error)?.message, false),
+  };
+
+  return OperationOutput.fromJSON({ error: errorDetails }) as OperationOutput;
+}
+
+
+export enum FailedExecutionErrorSource {
+  // Source of the error not known.
+  Unknown = 'unknown',
+  // Error is returned from the developer in the function.
+  DeveloperFunction = 'developer_function',
+  // Error is returned from the platform.
+  Platform = 'platform',
+}
+
+export type FailedExecutionError = {
+  source: FailedExecutionErrorSource;
+  error: FunctionExecutionError;
+};
+
+function createExecuteOperationResult(
+  responseData: any,
+  format: ExecuteOperationResult_SerializationFormat,
+): ExecuteOperationResult {
+  let data: string;
+
+  try {
+    switch (format) {
+      case ExecuteOperationResult_SerializationFormat.Proto:
+        const uint8array: Uint8Array = OperationOutput.encode(responseData).finish();
+        data = Buffer.from(uint8array).toString('base64');
+        break;
+      case ExecuteOperationResult_SerializationFormat.JSON:
+        data = Buffer.from(JSON.stringify(responseData)).toString('base64');
+        break;
+      default:
+        throw new Error(`Unsupported serialization format: ${format}`);
+    }
+  } catch (error) {
+    console.error('Error creating operation result:', error);
+    throw new Error('Failed to create operation result');
+  }
+
+  return {
+    serialization_format: format,
+    data: data,
+  } as ExecuteOperationResult;
+}
+
+function getOperationExecutionOperationMethod(event: any): string | undefined {
+  return event.execution_metadata.operation_method;
 }
